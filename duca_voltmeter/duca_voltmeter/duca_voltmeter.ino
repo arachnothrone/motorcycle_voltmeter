aaedcb4d79b7d6ca6b39fdd0c7af1292bb22b543
arachnothrone <arachnothrone@gmail.com>
2023-06-23 22:09:16 -0400

    Initial for Nano

---
+++ duca_voltmeter/duca_voltmeter/duca_voltmeter.ino
@@ -0,0 +1,82 @@
+
+
+#include <Wire.h> 
+#include <U8x8lib.h>
+
+int analogInput = A0;
+float vout = 0.0;
+float vin = 0.0;
+float R1 = 100000.0;  // R1 = 100K  - input V+
+float R2 = 10000.0;   // R2 = 10K   - input V-
+int value = 0;
+
+U8X8_SSD1306_128X32_UNIVISION_HW_I2C oled(/* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
+
+
+// void clockDisplayFunc(TimeDateStorage* tds)
+// {
+//   /* Display clock values on the screen */
+  
+//   printRtcValueOnDisplay(0, 0, tds->hrs);
+
+//   printRtcValueOnDisplay(3, 0, tds->min);
+
+//   printRtcValueOnDisplay(6, 0, tds->day);
+//   oled.print("-");
+
+//   printRtcValueOnDisplay(9, 0, tds->mnth);
+//   oled.print("-");
+
+//   oled.setCursor(12, 0);
+//   oled.print(tds->year);
+// }
+
+void printValueOnDisplay(uint8_t xPos, uint8_t yPos, uint8_t value)
+{
+  oled.setCursor(0, yPos);
+  oled.print("              $");
+  oled.setCursor(xPos, yPos);
+  oled.print(value);
+}
+
+void printVoltageOnDisplay(uint8_t xPos, uint8_t yPos, float value)
+{
+  oled.setCursor(0, yPos);
+  oled.print("              *");
+  oled.setCursor(xPos, yPos);
+  
+  oled.print(value);
+  // oled.print("/");
+  // oled.print(int(value / 1000.0));
+  // oled.print(".");
+  // int fract_part = (int)(value / 10.0) % 100;
+  // if (fract_part < 10) {oled.print("0");}
+  // oled.print(fract_part);
+}
+
+
+void setup() {
+  pinMode(/*A0*/analogInput, INPUT);                       // Analog input: Voltage 0 - 90V
+
+  Wire.begin();
+
+  // Set up OLED display
+  oled.begin();
+  oled.setPowerSave(0);
+  oled.setFont(u8x8_font_chroma48medium8_r); // u8x8_font_chroma48medium8_r u8x8_font_px437wyse700a_2x2_r u8g2_font_courB12_tf 
+  oled.setContrast(11);
+
+}
+
+void loop() {
+  value = analogRead(/*A0*/analogInput);
+  vout = (value * 5.0) / 1024.0;
+  vin = vout / (R2 / (R1 + R2));
+  if (vin < 0.01)
+  {
+    vin = 0.0;
+  }
+  printVoltageOnDisplay(3, 1, vin);
+  printValueOnDisplay(5, 3, value);
+  delay(300);
+}
